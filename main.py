bfb9e77a355c05fad42a63c20ffb4a3603d3c6d9
rachit1601 <rachit1601@users.noreply.github.com>
2021-07-17 08:44:28 +0000

    [create-pull-request] automated change

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ try:
     from dotenv import load_dotenv
     load_dotenv()
     TOKEN = os.getenv('TOKEN')
-except:
+except BaseException:
     import config
-    TOKEN=config.TOKEN
+    TOKEN = config.TOKEN
 
 
-#test2
+# test2
 
 
 bot = commands.Bot(command_prefix='>', description="ReMod Bot")
@@ -29,7 +29,9 @@ async def on_ready():
 
 @bot.command()
 async def mute(ctx, user: discord.Member):
-    role = discord.utils.find(lambda r: r.name == 'mods',ctx.message.guild.roles)
+    role = discord.utils.find(
+        lambda r: r.name == 'mods',
+        ctx.message.guild.roles)
     if role in ctx.message.author.roles:
         roleobject = discord.utils.get(
             ctx.message.guild.roles,
@@ -39,23 +41,28 @@ async def mute(ctx, user: discord.Member):
     else:
         await ctx.reply("You don't have permission to do that")
 
+
 @bot.command()
 async def unmute(ctx, user: discord.Member):
-    role = discord.utils.find(lambda r: r.name == 'mods',ctx.message.guild.roles)
+    role = discord.utils.find(
+        lambda r: r.name == 'mods',
+        ctx.message.guild.roles)
     if role in ctx.message.author.roles:
         roleobject = discord.utils.get(
             ctx.message.guild.roles,
             id=865254696259551233)
         await user.remove_roles(roleobject)
         await ctx.send(f":white_check_mark: Unmuted {user}")
-        
+
     else:
         await ctx.reply("You don't have permission to do that")
 
+
 @bot.command()
 async def gitpull(ctx):
-    if ctx.message.author.id in [744224056966119565,686483505252925533]:
-        subprocess.run(["git", "pull"], check=True, stdout=subprocess.PIPE).stdout
+    if ctx.message.author.id in [744224056966119565, 686483505252925533]:
+        subprocess.run(["git", "pull"], check=True,
+                       stdout=subprocess.PIPE).stdout
         await ctx.reply("Executing `git pull`")
     else:
         await ctx.reply("Sorry you don't have permission to do that")
@@ -63,18 +70,17 @@ async def gitpull(ctx):
 
 @bot.command()
 async def restart(ctx):
-    if ctx.message.author.id in [744224056966119565,686483505252925533]:
+    if ctx.message.author.id in [744224056966119565, 686483505252925533]:
         await ctx.reply("Restarting...`")
         print("Restarting...")
         os.system("python main.py")
         exit()
     else:
         await ctx.reply("Sorry you don't have permission to do that")
-    
+
+
 @bot.command()
 async def ping(ctx):
-    await ctx.send(f'My ping is {bot.latency*1000}ms !')    
+    await ctx.send(f'My ping is {bot.latency*1000}ms !')
 
 bot.run(TOKEN)
-
-
