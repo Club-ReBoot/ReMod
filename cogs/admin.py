0e3713969841f8771a6e88e110acb9aeb78ff465
rachit1601 <70265590+rachit1601@users.noreply.github.com>
2021-07-21 15:42:23 +0530

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -4,8 +4,8 @@ class general(commands.Cog, name='Administration',description="Some commands tha
     def __init__(self, bot):
         self.bot = bot
   
-    @commands.command(brief='Execute git pull', description='This command will execute git pull request. This command can only be run by bot owners')
-    async def gitpull(ctx):
+    @commands.command(pass_context=True,brief='Execute git pull', description='This command will execute git pull request. This command can only be run by bot owners')
+    async def gitpull(self,ctx):
         if ctx.message.author.id in [744224056966119565, 686483505252925533]:
             subprocess.run(["git", "pull"], check=True,
                            stdout=subprocess.PIPE).stdout
@@ -14,8 +14,8 @@ class general(commands.Cog, name='Administration',description="Some commands tha
             await ctx.reply("Sorry you don't have permission to do that")
 
 
-    @commands.command(brief='Restart the bot', description='This command will restart the bot. This command can only be run by bot owners')
-    async def restart(ctx):
+    @commands.command(pass_context=True,brief='Restart the bot', description='This command will restart the bot. This command can only be run by bot owners')
+    async def restart(self,ctx):
         if ctx.message.author.id in [744224056966119565, 686483505252925533]:
             await ctx.reply("Restarting...`")
             print("Restarting...")
